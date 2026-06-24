9197a3563dfa74b488399b2392f6b2213d27222d
Mythra Rajasekar <147605932+mythrarajasekar@users.noreply.github.com>
2026-06-24 16:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (72.71%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Solution{
+    public static int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Peak element index: " + findPeakElement(nums));
+
+        sc.close();
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
