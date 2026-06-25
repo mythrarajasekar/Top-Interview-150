aab7e1acd5b3f54d12a79a10b3c2a33b64e83d2e
Mythra Rajasekar <147605932+mythrarajasekar@users.noreply.github.com>
2026-06-25 14:02:24 +0530

    Time: 645 ms (33.93%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT tweet_id FROM Tweets 
+WHERE CHAR_LENGTH(content)>15;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
