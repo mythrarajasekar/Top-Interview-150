cbe296499c239d6920b35e70161620b714e6ac7f
Mythra Rajasekar <147605932+mythrarajasekar@users.noreply.github.com>
2026-07-06 10:26:36 +0530

    Time: 2 ms (92.13%) | Memory: 47.8 MB (56.63%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int k = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] != 0){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        for(int i = k;i<nums.length;i++){
+            nums[i] = 0;
+        }
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
