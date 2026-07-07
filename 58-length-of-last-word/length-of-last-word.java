69bb432f4f201fe8aeb13948ce5ac049061f5c32
Mythra Rajasekar <147605932+mythrarajasekar@users.noreply.github.com>
2026-07-07 16:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (17.25%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int len = 0;
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            len++;
+            i--;
+        }
+
+        return len;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
