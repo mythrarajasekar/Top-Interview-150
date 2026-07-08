027acefffb09cae118fb51b51d605d8d437af792
Mythra Rajasekar <147605932+mythrarajasekar@users.noreply.github.com>
2026-07-08 14:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (92.19%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,23 +1,24 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] == target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right){
+            int mid  = left + (right-left)/2;
+
+            if(nums[mid] ==  target){
                 return mid;
-            } else if (nums[mid] < target) {
+            }
+            else if (nums[mid]< target){
                 left = mid + 1;
-            } else {
+            }
+            else {
                 right = mid - 1;
             }
+            
         }
+
+        return left;
         
-        return left; // Position where target should be inserted
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
